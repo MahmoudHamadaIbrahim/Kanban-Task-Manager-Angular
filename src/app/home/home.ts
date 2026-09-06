b57bf96560a053b35aaa2897a2bc190dcda38ba5
MahmoudHamadaIbrahim <wnttlt6@gmail.com>
2026-09-06 10:42:48 +0300

    refactor: update home.ts to improve performance and readability

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -42,6 +42,7 @@ export class Home {
     this.selectedIndex = index;
     this.modal.setEditData(task, index);
     this.modal.openModal();
+    this.modal.resetForm();
   }
 
   deleteTask(task: Task) {
